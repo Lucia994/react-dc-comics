33f50904b2dff0982ee9512d7706b8293ea7d510
Lucia994 <l.summa94@gmail.com>
2025-07-14 18:08:51 +0200

    Add img folder to src folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import dcLogo from "./img/dc-logo.png"
+
 /*1. Define the compotenent*/
 export default function Header() {
     return (
 
         < Header >
+            <img src={dcLogo} />
 
         </Header >
 
