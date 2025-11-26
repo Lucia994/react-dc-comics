b31105fd3b1db262203e50dd167f3c90570309f9
Lucia994 <l.summa94@gmail.com>
2025-11-26 18:33:11 +0100

    Add jumbotron section and import image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 import bcDigitalComics from "../assets/img/buy-comics-digital-comics.png";
-import bcMerchandise from "../assets//img/buy-comics-merchandise.png";
-import bcShopLocator from "../assets//img/buy-comics-shop-locator.png";
-import bcSubscriptions from "../assets//img/buy-comics-subscriptions.png";
-import dcPowerVisa from "../assets//img/buy-dc-power-visa.svg";
+import bcMerchandise from "../assets/img/buy-comics-merchandise.png";
+import bcShopLocator from "../assets/img/buy-comics-shop-locator.png";
+import bcSubscriptions from "../assets/img/buy-comics-subscriptions.png";
+import dcPowerVisa from "../assets/img/buy-dc-power-visa.svg";
+import jumbotron from "../assets/img/jumbotron.jpg";
 
 export default function Main() {
   return (
     <main>
+      <section className="sectionJumbotron">
+        <img src={jumbotron} />
+      </section>
       <div className="sectionFirst flex">--&gt;Content goes here&lt;--</div>
       <div className="sectionSecond flex j-content-between align-items-center">
         <div className="item">
