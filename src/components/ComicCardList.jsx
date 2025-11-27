792366a3b9bd22c89f0a9291aa5f529218570784
Lucia994 <l.summa94@gmail.com>
2025-11-27 16:03:15 +0100

    Import ComicCardList into Main component

--- src/components/ComicCardList.jsx
+++ src/components/ComicCardList.jsx
@@ -2,14 +2,12 @@ import { comics } from "../data/comics";
 import ComicCard from "./ComicCard";
 
 export default function ComicCardList() {
+  console.log(comics);
   return (
     <>
       <ul className="card flex j-content-between align-items-center">
         {comics.map((comic) => (
-          <ComicCard key={comic.id}>
-            thumb={comic.thumb}
-            title={comic.title}
-          </ComicCard>
+          <ComicCard key={comic.id} thumb={comic.thumb} title={comic.title} />
         ))}
         ;
       </ul>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,14 +5,15 @@ import bcSubscriptions from "../assets/img/buy-comics-subscriptions.png";
 import dcPowerVisa from "../assets/img/buy-dc-power-visa.svg";
 import jumbotron from "../assets/img/jumbotron.jpg";
 import { comics } from "../data/comics";
+import ComicCardList from "./ComicCardList";
 
 const itemsMenuMain = [
   {
-    src: "../assets/img/buy-comics-digital-comics.png",
+    src: bcDigitalComics,
     title: "DIGITAL COMICS",
   },
   {
-    src: "../assets/img/buy-comics-merchandise.png",
+    src: "/imgs/buy-comics-merchandise.png",
     title: "DC MERCHANDISE",
   },
   {
@@ -37,16 +38,7 @@ export default function Main() {
       </section>
       <section className="sectionFirst flex">
         {/*Insert item cards*/}
-        <section>
-          {/* <ul className="card flex j-content-between align-items-center">
-            {comics.map((comic) => (
-              <li key={comic.id}>
-                <img src={comic.thumb} />
-                <h5>{comic.title}</h5>
-              </li>
-            ))}
-          </ul> */}
-        </section>
+        <ComicCardList />
       </section>
       <div className="sectionSecond">
         <div className="item">
